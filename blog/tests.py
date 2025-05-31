4a3c439607ebebf76dd294f44084cac6625826ba
Ricky <diwasbasyal1417@gmail.com>
2025-05-31 11:45:38 +1000

    Add unit tests for home, dashboard, and login views

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class BasicTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username="testuser", password="testpass")
+
+    def test_home_page_status_code(self):
+        response = self.client.get(
+            reverse("home")
+        )  # adjust if your URL name is different
+        self.assertEqual(response.status_code, 200)
+
+    def test_dashboard_requires_login(self):
+        response = self.client.get(reverse("dashboard"))
+        self.assertRedirects(response, "/login/?next=/dashboard/")
+
+    def test_login_with_valid_credentials(self):
+        login = self.client.login(username="testuser", password="testpass")
+        self.assertTrue(login)
