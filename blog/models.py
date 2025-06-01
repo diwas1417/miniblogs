2756db2fc6e48faee661d9a630e47c9b1cd32582
Ricky <diwasbasyal1417@gmail.com>
2025-06-01 21:34:37 +1000

    Fix: Disable SonarCloud auto analysis and trigger CI

--- blog/models.py
+++ blog/models.py
@@ -4,15 +4,13 @@ Defines database models for users and posts in the application.
 """
 
 from django.db import models
+from django.contrib.auth.models import User
 
 
+# Create your models here.
 class Post(models.Model):
     """Model representing a blog post."""
 
-    title = models.CharField(max_length=100)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
     desc = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        """Return the title of the post."""
-        return self.title
